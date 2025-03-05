812b069730cbb000de7b247b98b3ea318d20fa74
AbominablePutridity <qq594492@gmail.com>
2025-03-05 20:22:53 +0300

    Добавление поля поиска на главной странице прилодения (Запускать бэк на localhost:8000)

--- Front/AvaloniaWebApp/AvaloniaApp1/Views/MainPage.axaml.cs
+++ Front/AvaloniaWebApp/AvaloniaApp1/Views/MainPage.axaml.cs
@@ -5,14 +5,17 @@ using Avalonia.Markup.Xaml;
 using System;
 using System.Threading.Tasks;
 using Tmds.DBus.Protocol;
+using Avalonia.Interactivity;
 
 namespace Views;
 
 public partial class MainPage : UserControl
 {
     private HttpClient _httpClient;
-    private GetProductsService _product;   
-    string apiUrl = "products"; 
+    private GetProductsService _product;  
+    string searchFilter = "";
+
+    string apiUrl = "products?page=1&amountOfPages=6"; 
 
     public MainPage()
     {
@@ -27,8 +30,20 @@ public partial class MainPage : UserControl
 
     private async void InitializeAsync()
     {
+        if ((searchFilter != "") || (searchFilter != null)) {
+            apiUrl = apiUrl + "?page=1&amountOfPages=6&author=" + searchFilter;
+        }
+
         string result = await _product.LoadDataAsync(apiUrl);
         TBlock.Text = result;
     }
     
+    private void Button_Click_Enter(object sender, RoutedEventArgs e)
+    {
+        // Получение текста из TextBox
+        searchFilter = SearchTB.Text;
+        //Console.WriteLine(searchFilter);
+
+        InitializeAsync();
+    }
 }
